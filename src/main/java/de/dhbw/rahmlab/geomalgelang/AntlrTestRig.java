dca5f0a09d56ad38c5a9bd2c8fb40519d675d58f
codedeedope <26957536+codedeedope@users.noreply.github.com>
2022-01-21 14:02:06 +0100

    visual debug helper works now

---
+++ src/main/java/de/dhbw/rahmlab/geomalgelang/AntlrTestRig.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dhbw.rahmlab.geomalgelang;
+
+import org.antlr.v4.gui.TestRig;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Parser;
+
+/**
+ * https://github.com/antlr/antlr4/blob/4.9.3/tool/src/org/antlr/v4/gui/TestRig.java
+ *
+ * @author fabian
+ */
+public class AntlrTestRig extends TestRig {
+
+    public AntlrTestRig() throws Exception {
+        super(new String[2]);
+
+        // Not needed
+        super.grammarName = null;
+        super.inputFiles.clear();
+        super.encoding = null;
+        super.psFile = null;
+
+        // Optional
+        super.printTree = false;
+        super.trace = false;
+        super.diagnostics = false;
+        super.showTokens = false;
+
+        // Standard
+        super.gui = true;
+
+        // To be set somewhere else
+        super.startRuleName = null;
+    }
+
+    public void useGui(boolean value) {
+        super.gui = value;
+    }
+
+    public void useStdOut(boolean value) {
+        super.printTree = value;
+        super.trace = value;
+        super.diagnostics = value;
+        super.showTokens = value;
+    }
+
+    public void process(Lexer lexer, Parser parser, CharStream input, String startRuleName) throws Exception {
+        super.startRuleName = startRuleName;
+        super.process(lexer, parser.getClass(), parser, input);
+    }
+}
--- src/main/java/de/dhbw/rahmlab/geomalgelang/App.java
+++ src/main/java/de/dhbw/rahmlab/geomalgelang/App.java
@@ -5,23 +5,10 @@
  */
 package de.dhbw.rahmlab.geomalgelang;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStreams;
 
 public class App {
-
-    public static class MyProgramListener extends GeomAlgeParserBaseListener {
-
-        @Override
-        public void enterExpr(GeomAlgeParser.ExprContext ctx) {  //see gramBaseListener for allowed functions
-            System.out.println("rule entered: " + ctx.getText());      //code that executes per rule
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         CharStream inputStream = CharStreams.fromString(
@@ -30,13 +17,8 @@ public class App {
         CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
         GeomAlgeParser parser = new GeomAlgeParser(commonTokenStream);
 
-        MyProgramListener listener = new MyProgramListener();   // your custom extension from BaseListener
-        parser.addParseListener(listener);
-        parser.program().enterRule(listener);    // myProgramStart is your grammar rule to parse
-
-        // what we had built?
-        //MyProgram myProgramInstance = listener.getMyProgram();    // in your listener implementation populate a MyProgram instance
-        //System.out.println(myProgramInstance.toString());
+        AntlrTestRig testRig = new AntlrTestRig();
+        testRig.process(lexer, parser, inputStream, "program");
 
         /*
         GeomAlgeParser.ExprContext context = parser.expr();
