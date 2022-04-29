641f6f10a9bc14f74e04d54bbc5ec528b65d5483
codedeedope <26957536+codedeedope@users.noreply.github.com>
2022-04-29 23:30:50 +0200

    optimized AstStringBuilder

--- src/main/java/de/dhbw/rahmlab/geomalgelang/parsing/debug/AstStringBuilder.java
+++ src/main/java/de/dhbw/rahmlab/geomalgelang/parsing/debug/AstStringBuilder.java
@@ -5,9 +5,7 @@
 package de.dhbw.rahmlab.geomalgelang.parsing.debug;
 
 import com.oracle.truffle.api.nodes.Node;
-import de.dhbw.rahmlab.geomalgelang.truffle.nodes.GlobalVariableReference;
 import java.util.ArrayDeque;
-import java.util.Deque;
 
 /**
  *
@@ -21,46 +19,53 @@ public abstract class AstStringBuilder {
 
 	private static final String INDENTATION_SYMBOL = "	";
 
-	public static String getAstString(Node root) {
-		StringBuilder stringBuilder = new StringBuilder();
+	public static String getAstString(final Node root) {
+		final StringBuilder stringBuilder = new StringBuilder();
+		final ArrayDeque<IdentNode> identNodes = new ArrayDeque<>();
+		// Outside of the loop for optimization
+		final ArrayDeque<Node> childrenReverse = new ArrayDeque<>();
 
-		Deque<IdentNode> identNodes = new ArrayDeque<>();
-
-		IdentNode rootIdentNode = new IdentNode(root, "");
+		final IdentNode rootIdentNode = new IdentNode(root, "");
 		identNodes.push(rootIdentNode);
 
-		// Outside of the loop for optimization
-		Deque<Node> children = new ArrayDeque<>();
-
 		while (!identNodes.isEmpty()) {
-			children.clear();
-
-			IdentNode frontIdentNode = identNodes.pop();
-			Node currentNode = frontIdentNode.node;
-			String currentIndentation = frontIdentNode.indentation;
+			final IdentNode frontIdentNode = identNodes.pop();
+			final Node currentNode = frontIdentNode.node;
+			final String currentIndentation = frontIdentNode.indentation;
 
 			String name = currentNode.getClass().getSimpleName().replace("NodeGen", "");
 
+			/*
+			// Only needed for debugging
 			if (currentNode instanceof GlobalVariableReference) {
 				GlobalVariableReference theCurrent = (GlobalVariableReference) currentNode;
 				name = "GlobalVariableReference[" + theCurrent.getName() + "]";
 			}
-
+			 */
 			stringBuilder.append(currentIndentation);
 			stringBuilder.append(name);
 			stringBuilder.append("\n");
 
-			// This is to reverse the order of the children.
-			for (Node child : currentNode.getChildren()) {
-				children.push(child);
-			}
-			// Pushing them into the identNode will reverse them a second time.
-			// So overall we get the first child on the first position of identNodes. Then the next child.
-			// Till the last child. And after the children all previously inserted nodes.
-			String childrenIdentation = currentIndentation + INDENTATION_SYMBOL;
-			for (Node child : children) {
-				IdentNode childIdentNode = new IdentNode(child, childrenIdentation);
-				identNodes.push(childIdentNode);
+			final Iterable<Node> childrenIterable = currentNode.getChildren();
+
+			// Leaf, if not
+			if (childrenIterable.iterator().hasNext()) {
+				childrenReverse.clear();
+
+				// This reverses the order of the children.
+				for (Node child : childrenIterable) {
+					childrenReverse.push(child);
+				}
+
+				// Pushing the children into the identNode will reverse them a second time.
+				// So overall we get the first child on the first position of identNodes. Then the next child.
+				// Till the last child. And after the children all previously inserted nodes.
+				// So essentially the tree will get sorted topologically in a way such that traversing it in the according direction will be equivalent to a pre-order traversal.
+				final String childrenIdentation = currentIndentation + INDENTATION_SYMBOL;
+				for (Node child : childrenReverse) {
+					final IdentNode childIdentNode = new IdentNode(child, childrenIdentation);
+					identNodes.push(childIdentNode);
+				}
 			}
 		}
 
