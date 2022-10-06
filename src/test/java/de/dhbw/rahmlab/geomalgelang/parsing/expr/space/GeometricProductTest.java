a507594fc601eb8edc5c84b846c13ce756e56457
codedeedope <26957536+codedeedope@users.noreply.github.com>
2022-10-06 13:48:00 +0200

    GeometricProductTest with unOp

--- src/test/java/de/dhbw/rahmlab/geomalgelang/parsing/expr/space/GeometricProductTest.java
+++ src/test/java/de/dhbw/rahmlab/geomalgelang/parsing/expr/space/GeometricProductTest.java
@@ -4,6 +4,7 @@
  */
 package de.dhbw.rahmlab.geomalgelang.parsing.expr.space;
 
+import de.dhbw.rahmlab.geomalgelang.parsing.GeomAlgeParser;
 import de.dhbw.rahmlab.geomalgelang.parsing.ParsingService;
 import de.dhbw.rahmlab.geomalgelang.parsing._util.AstStringBuilder;
 import de.dhbw.rahmlab.geomalgelang.truffle.nodes.binaryOps.*;
@@ -30,33 +31,83 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GeometricProductTest {
 
-	public record ExampleSet(String ExprType, String nodeName, List<String> examples) {
+	public record ExampleSet(String exprType, String nodeName, List<String> examples) {
 
 	}
 
-	ExampleSet parenExpr = new ExampleSet("paren", GlobalVariableReference.class.getSimpleName(), List.of(new String[]{"(a)"}));
+	static final ExampleSet parenExpr = new ExampleSet("paren", GlobalVariableReference.class.getSimpleName(), List.of(new String[]{"(a)"}));
 	//ExampleSet funcExpr = new ExampleSet("func", "function", List.of(new String[]{"abs(2.0)"})); //Gibt es noch nicht
-	ExampleSet composExpr = new ExampleSet("compos", GradeExtraction.class.getSimpleName(), List.of(new String[]{"<a>₀"}));
-	ExampleSet varExpr = new ExampleSet("var", GlobalVariableReference.class.getSimpleName(), List.of(new String[]{"a"}));
-	ExampleSet constExpr = new ExampleSet("const", Constant.class.getSimpleName(), List.of(new String[]{"π"}));
-	ExampleSet numExpr = new ExampleSet("num", ScalarLiteral.class.getSimpleName(), List.of(new String[]{"2.0"}));
+	static final ExampleSet composExpr = new ExampleSet("compos", GradeExtraction.class.getSimpleName(), List.of(new String[]{"<a>₀"}));
+	static final ExampleSet varExpr = new ExampleSet("var", GlobalVariableReference.class.getSimpleName(), List.of(new String[]{"a"}));
+	static final ExampleSet constExpr = new ExampleSet("const", Constant.class.getSimpleName(), List.of(new String[]{"π"}));
+	static final ExampleSet numExpr = new ExampleSet("num", ScalarLiteral.class.getSimpleName(), List.of(new String[]{"2.0"}));
 
-	List<ExampleSet> atomicExpr = List.of(new ExampleSet[]{parenExpr, composExpr, varExpr, constExpr, numExpr});
+	static final List<ExampleSet> atomicExpr = List.of(new ExampleSet[]{parenExpr, composExpr, varExpr, constExpr, numExpr});
 
-	public GeometricProductTest() {
-		//
+	static ArrayList<String> generateSpaces(int maxSpaces) {
+		ArrayList<String> spaces = new ArrayList(maxSpaces);
+		for (int i = 1; i < maxSpaces; ++i) {
+			spaces.add(" ".repeat(i));
+		}
+		return spaces;
 	}
+
+	static final ArrayList<String> spaces = generateSpaces(3);
+
+	// changes later
+	static final String unOpLSymbol = "−";
+
+	static ExampleSet generateUnOpLExpr() {
+		ArrayList<String> unOpLExamples = new ArrayList();
+		for (ExampleSet oneAtomicExpr : atomicExpr) {
+			for (String example : oneAtomicExpr.examples()) {
+				String fullExpr = unOpLSymbol + example; //Später -
+				unOpLExamples.add(fullExpr);
+			}
+		}
+
+		ExampleSet unOpLExpr = new ExampleSet("unOpL", Negate.class.getSimpleName(), unOpLExamples);
+		return unOpLExpr;
+	}
+
+	static final String unOpRSymbol = "˜";
+
+	static ExampleSet generateUnOpRExpr() {
+		ArrayList<String> unOpRExamples = new ArrayList();
+		for (ExampleSet oneAtomicExpr : atomicExpr) {
+			for (String example : oneAtomicExpr.examples()) {
+				String fullExpr = example + unOpRSymbol;
+				unOpRExamples.add(fullExpr);
+			}
+		}
+
+		ExampleSet unOpRExpr = new ExampleSet("unOpR", Reverse.class.getSimpleName(), unOpRExamples);
+		return unOpRExpr;
+	}
+
+	static final ExampleSet unOpLExpr = generateUnOpLExpr();
+	static final ExampleSet unOpRExpr = generateUnOpRExpr();
+
+	static List<ExampleSet> generateAllExpr() {
+		ArrayList<ExampleSet> allExprList = new ArrayList();
+		allExprList.addAll(atomicExpr);
+		allExprList.add(unOpLExpr);
+		allExprList.add(unOpRExpr);
+		return allExprList;
+	}
+
+	static final List<ExampleSet> allExpr = generateAllExpr();
 
 	Context context;
 
 	@BeforeAll
-	public void setup() {
+	void setup() {
 		context = Context.create();
 		context.enter();
 	}
 
 	@AfterAll
-	public void desetup() {
+	void desetup() {
 		context.leave();
 		context.close();
 	}
@@ -65,19 +116,13 @@ public class GeometricProductTest {
 	// Genau 1 Leerzeichen zwischen zwei aus {unOp, grouping, lit}-Expr wird als binOp_ interpretiert.
 	@Test
 	void R1_1_and_R1_2() throws UnsupportedEncodingException {
-		final int maxSpaces = 3;
-		ArrayList<String> spaces = new ArrayList(maxSpaces);
-		for (int i = 1; i < maxSpaces; ++i) {
-			spaces.add(" ".repeat(i));
-		}
-
 		StringBuilder programStringBuilder = new StringBuilder();
 		StringBuilder expectedAstStringBuilder = new StringBuilder();
 
-		for (ExampleSet leftExamples : atomicExpr) {
+		for (ExampleSet leftExamples : allExpr) {
 			String leftNodeName = leftExamples.nodeName();
 
-			for (ExampleSet rightExamples : atomicExpr) {
+			for (ExampleSet rightExamples : allExpr) {
 				String rightNodeName = rightExamples.nodeName();
 
 				expectedAstStringBuilder.setLength(0);
@@ -89,9 +134,9 @@ public class GeometricProductTest {
 				expectedAstStringBuilder.append(rightNodeName);
 				expectedAstStringBuilder.append("\n");
 
-				for (String space : spaces) {
-					for (String left : leftExamples.examples()) {
-						for (String right : rightExamples.examples()) {
+				for (String left : leftExamples.examples()) {
+					for (String right : rightExamples.examples()) {
+						for (String space : spaces) {
 							programStringBuilder.setLength(0);
 							programStringBuilder.append(left);
 							programStringBuilder.append(space);
@@ -110,6 +155,13 @@ public class GeometricProductTest {
 							}
 
 							System.out.print(messageBuilder.toString());
+							/*
+							System.out.print("->expected:");
+							System.out.print(expectedAstStringBuilder.toString());
+							System.out.print("->got:");
+							System.out.print(actualAstString);
+							System.out.println("---");
+							 */
 							Assertions.assertEquals(expectedAstStringBuilder.toString(), actualAstString, messageBuilder.toString());
 						}
 					}
