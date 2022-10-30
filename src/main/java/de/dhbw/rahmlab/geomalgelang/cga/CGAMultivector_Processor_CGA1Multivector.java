427134704fc53305d6e551724a76cdc31a12dfa4
codedeedope <26957536+codedeedope@users.noreply.github.com>
2022-10-30 15:18:02 +0100

    update compatiblity to newest version of dependency ConformalGeometricAlgebra

--- src/main/java/de/dhbw/rahmlab/geomalgelang/cga/CGAMultivector_Processor_CGA1Multivector.java
+++ src/main/java/de/dhbw/rahmlab/geomalgelang/cga/CGAMultivector_Processor_CGA1Multivector.java
@@ -5,6 +5,7 @@
 package de.dhbw.rahmlab.geomalgelang.cga;
 
 import de.orat.math.cga.impl1.CGA1Multivector;
+import de.orat.math.cga.spi.iCGAMultivector;
 import static de.orat.math.ga.basis.InnerProductTypes.LEFT_CONTRACTION;
 import static de.orat.math.ga.basis.InnerProductTypes.RIGHT_CONTRACTION;
 
@@ -12,139 +13,139 @@ import static de.orat.math.ga.basis.InnerProductTypes.RIGHT_CONTRACTION;
  *
  * @author fabian
  */
-public class CGAMultivector_Processor_CGA1Multivector implements ICGAMultivector_Processor_Concrete<CGA1Multivector> {
+public class CGAMultivector_Processor_CGA1Multivector implements ICGAMultivector_Processor_Concrete<iCGAMultivector> {
 
 	// Binary Operators
 	@Override
-	public CGA1Multivector meet(CGA1Multivector lhs, CGA1Multivector rhs) {
+	public iCGAMultivector meet(iCGAMultivector lhs, iCGAMultivector rhs) {
 		return lhs.meet(rhs);
 	}
 
 	@Override
-	public CGA1Multivector join(CGA1Multivector lhs, CGA1Multivector rhs) {
-		return lhs.join(rhs);
-	}
-
-	@Override
-	public CGA1Multivector geometric_product(CGA1Multivector lhs, CGA1Multivector rhs) {
-		return lhs.gp(rhs);
-	}
-
-	@Override
-	public CGA1Multivector inner_product(CGA1Multivector lhs, CGA1Multivector rhs) {
-		return lhs.ip(rhs, RIGHT_CONTRACTION);
-	}
-
-	@Override
-	public CGA1Multivector right_contraction(CGA1Multivector lhs, CGA1Multivector rhs) {
-		return lhs.ip(rhs, RIGHT_CONTRACTION);
-	}
-
-	@Override
-	public CGA1Multivector left_contraction(CGA1Multivector lhs, CGA1Multivector rhs) {
-		return lhs.ip(rhs, LEFT_CONTRACTION);
-	}
-
-	@Override
-	public CGA1Multivector outer_product(CGA1Multivector lhs, CGA1Multivector rhs) {
+	public iCGAMultivector join(iCGAMultivector lhs, iCGAMultivector rhs) {
 		return lhs.op(rhs);
 	}
 
 	@Override
-	public CGA1Multivector regressive_product(CGA1Multivector lhs, CGA1Multivector rhs) {
+	public iCGAMultivector geometric_product(iCGAMultivector lhs, iCGAMultivector rhs) {
+		return lhs.gp(rhs);
+	}
+
+	@Override
+	public iCGAMultivector inner_product(iCGAMultivector lhs, iCGAMultivector rhs) {
+		return lhs.ip(rhs, RIGHT_CONTRACTION);
+	}
+
+	@Override
+	public iCGAMultivector right_contraction(iCGAMultivector lhs, iCGAMultivector rhs) {
+		return lhs.ip(rhs, RIGHT_CONTRACTION);
+	}
+
+	@Override
+	public iCGAMultivector left_contraction(iCGAMultivector lhs, iCGAMultivector rhs) {
+		return lhs.ip(rhs, LEFT_CONTRACTION);
+	}
+
+	@Override
+	public iCGAMultivector outer_product(iCGAMultivector lhs, iCGAMultivector rhs) {
+		return lhs.op(rhs);
+	}
+
+	@Override
+	public iCGAMultivector regressive_product(iCGAMultivector lhs, iCGAMultivector rhs) {
 		return lhs.vee(rhs);
 	}
 
 	@Override
-	public CGA1Multivector division(CGA1Multivector lhs, CGA1Multivector rhs) {
-		return lhs.div(rhs);
+	public iCGAMultivector division(iCGAMultivector lhs, iCGAMultivector rhs) {
+		return lhs.gp(rhs.generalInverse());
 	}
 
 	@Override
-	public CGA1Multivector addition(CGA1Multivector lhs, CGA1Multivector rhs) {
+	public iCGAMultivector addition(iCGAMultivector lhs, iCGAMultivector rhs) {
 		return lhs.add(rhs);
 	}
 
 	@Override
-	public CGA1Multivector subtraction(CGA1Multivector lhs, CGA1Multivector rhs) {
+	public iCGAMultivector subtraction(iCGAMultivector lhs, iCGAMultivector rhs) {
 		return lhs.sub(rhs);
 	}
 
 	// Unary Operators
 	@Override
-	public CGA1Multivector general_inverse(CGA1Multivector input) {
+	public iCGAMultivector general_inverse(iCGAMultivector input) {
 		return input.generalInverse();
 	}
 
 	@Override
-	public CGA1Multivector dual(CGA1Multivector input) {
+	public iCGAMultivector dual(iCGAMultivector input) {
 		return input.dual();
 	}
 
 	@Override
-	public CGA1Multivector undual(CGA1Multivector input) {
+	public iCGAMultivector undual(iCGAMultivector input) {
 		return input.undual();
 	}
 
 	@Override
-	public CGA1Multivector reverse(CGA1Multivector input) {
+	public iCGAMultivector reverse(iCGAMultivector input) {
 		return input.reverse();
 	}
 
 	@Override
-	public CGA1Multivector clifford_conjugate(CGA1Multivector input) {
+	public iCGAMultivector clifford_conjugate(iCGAMultivector input) {
 		return input.conjugate();
 	}
 
 	@Override
-	public CGA1Multivector exponentiate(CGA1Multivector input) {
+	public iCGAMultivector exponentiate(iCGAMultivector input) {
 		return input.exp();
 	}
 
 	@Override
-	public CGA1Multivector involute(CGA1Multivector input) {
+	public iCGAMultivector involute(iCGAMultivector input) {
 		return input.gradeInversion();
 	}
 
 	@Override
-	public CGA1Multivector negate(CGA1Multivector input) {
+	public iCGAMultivector negate(iCGAMultivector input) {
 		return (new CGA1Multivector(-1.0)).gp(input);
 	}
 
 	// Nullary Operators
 	@Override
-	public CGA1Multivector base_vector_origin() {
-		return CGA1Multivector.createBasisVectorE0();
+	public iCGAMultivector base_vector_origin() {
+		return (new CGA1Multivector()).createOrigin(1d);
 	}
 
 	@Override
-	public CGA1Multivector base_vector_infinity() {
-		return CGA1Multivector.createBasisVectorEinf();
+	public iCGAMultivector base_vector_infinity() {
+		return (new CGA1Multivector()).createInf(1d);
 	}
 
 	@Override
-	public CGA1Multivector base_vector_x() {
-		return CGA1Multivector.createBasisVector(1);
+	public iCGAMultivector base_vector_x() {
+		return (new CGA1Multivector()).createEx(1d);
 	}
 
 	@Override
-	public CGA1Multivector base_vector_y() {
-		return CGA1Multivector.createBasisVector(2);
+	public iCGAMultivector base_vector_y() {
+		return (new CGA1Multivector()).createEy(1d);
 	}
 
 	@Override
-	public CGA1Multivector base_vector_z() {
-		return CGA1Multivector.createBasisVector(3);
+	public iCGAMultivector base_vector_z() {
+		return (new CGA1Multivector()).createEz(1d);
 	}
 
 	// Other
 	@Override
-	public CGA1Multivector grade_extraction(CGA1Multivector input, int grade) {
+	public iCGAMultivector grade_extraction(iCGAMultivector input, int grade) {
 		return input.extractGrade(grade);
 	}
 
 	@Override
-	public CGA1Multivector create(double scalar) {
+	public iCGAMultivector create(double scalar) {
 		return new CGA1Multivector(scalar);
 	}
 
