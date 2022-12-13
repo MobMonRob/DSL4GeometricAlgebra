6db37fbc84776a797367f03ccf20e9c7af1adb27
codedeedope <26957536+codedeedope@users.noreply.github.com>
2022-12-13 15:38:54 +0100

    shrink

--- src/main/java/de/dhbw/rahmlab/geomalgelang/truffle/features/functionDefinitions/nodes/FunctionArgumentReader.java
+++ src/main/java/de/dhbw/rahmlab/geomalgelang/truffle/features/functionDefinitions/nodes/FunctionArgumentReader.java
@@ -3,7 +3,6 @@ package de.dhbw.rahmlab.geomalgelang.truffle.features.functionDefinitions.nodes;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import de.dhbw.rahmlab.geomalgelang.truffle.common.runtime.CgaTruffleBox;
-import de.dhbw.rahmlab.geomalgelang.truffle.common.runtime.GeomAlgeLangException;
 import de.orat.math.cga.api.CGAMultivector;
 
 public class FunctionArgumentReader extends Node {
@@ -15,22 +14,6 @@ public class FunctionArgumentReader extends Node {
 	}
 
 	public CGAMultivector executeReadFunctionArgument(VirtualFrame frame) {
-		Object[] args = frame.getArguments();
-
-		// class Function already ensures correct arity
-		Object arg = args[index];
-
-		if (arg instanceof Object[]) {
-			Object[] actualArgs = (Object[]) arg;
-			if (actualArgs.length > 0) {
-				Object actualArg = actualArgs[0];
-				if (actualArg instanceof CgaTruffleBox) {
-					CgaTruffleBox truffleBox = (CgaTruffleBox) actualArg;
-					return truffleBox.inner;
-				}
-			}
-		}
-
-		throw new GeomAlgeLangException("FunctionArgumentReader failed", this);
+		return ((CgaTruffleBox) (((Object[]) ((frame.getArguments())[index]))[0])).inner;
 	}
 }
