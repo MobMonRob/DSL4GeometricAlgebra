cd5d59f62b2a8a1edf930cc4a77eae465f2c7ba2
codedeedope <26957536+codedeedope@users.noreply.github.com>
2024-01-23 18:12:31 +0100

    Fix build.

--- GeomAlgeLang/src/main/java/de/dhbw/rahmlab/geomalgelang/_symbolicWithoutTruffle/api/Arguments.java
+++ GeomAlgeLang/src/main/java/de/dhbw/rahmlab/geomalgelang/_symbolicWithoutTruffle/api/Arguments.java
@@ -3,16 +3,17 @@ package de.dhbw.rahmlab.geomalgelang._symbolicWithoutTruffle.api;
 import de.orat.math.gacalc.api.ExprGraphFactory;
 import de.orat.math.gacalc.api.GAExprGraphFactoryService;
 import de.orat.math.gacalc.api.MultivectorNumeric;
+import de.orat.math.gacalc.spi.iEuclideanTypeConverter;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.jogamp.vecmath.Tuple3d;
 import util.cga.SparseCGAColumnVector;
-import util.cga.SparseCGAColumnVectorFactory;
 
 public class Arguments {
 
 	private final ExprGraphFactory exprGraphFactory = GAExprGraphFactoryService.getExprGraphFactoryThrowing();
+	private final iEuclideanTypeConverter euclideanTypeConverter = exprGraphFactory.getEuclideanTypeConverter();
 	private final Map<String, MultivectorNumeric> argsMap = new LinkedHashMap<>();
 	private final Map<String, MultivectorNumeric> argsMapView = Collections.unmodifiableMap(this.argsMap);
 
@@ -29,7 +30,7 @@ public class Arguments {
 	}
 
 	protected Arguments createPutReturn(String argName, SparseCGAColumnVector sparseVec) throws IllegalArgumentException {
-		var mvec = this.exprGraphFactory.createMultivectorNumeric(sparseVec.nonzeros(), sparseVec);
+		var mvec = this.exprGraphFactory.createMultivectorNumeric(sparseVec);
 		this.put(argName, mvec);
 		return this;
 	}
@@ -41,12 +42,12 @@ public class Arguments {
 	}
 
 	public Arguments euclidean_vector(String argName, Tuple3d tuple3d) {
-		var sparseVec = SparseCGAColumnVectorFactory.euclidean_vector(tuple3d);
+		var sparseVec = euclideanTypeConverter.euclidean_vector(tuple3d);
 		return createPutReturn(argName, sparseVec);
 	}
 
 	public Arguments scalar_ipns(String argName, double scalar) {
-		var sparseVec = SparseCGAColumnVectorFactory.scalar_ipns(scalar);
+		var sparseVec = euclideanTypeConverter.scalar_ipns(scalar);
 		return createPutReturn(argName, sparseVec);
 	}
 }
