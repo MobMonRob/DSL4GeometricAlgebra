e8ad2690eb2f9ea5c4301caad206d997fb4b279b
codedeedope <26957536+codedeedope@users.noreply.github.com>
2024-02-16 13:51:54 +0100

    Fixed thread-safety of double checked locking.

---
+++ GeomAlgeLang/src/main/java/de/dhbw/rahmlab/geomalgelang/_TrufflePrototype/ConcurrentLazyOfWeak.java
@@ -0,0 +1,42 @@
+package de.dhbw.rahmlab.geomalgelang._TrufflePrototype;
+
+import java.lang.ref.WeakReference;
+import java.util.function.Supplier;
+
+/**
+ * <pre>
+ * It is thread-safe.
+ * It assumes that reinitialisation is rare.
+ * If supplier.get() blocks, a deadlock could occur.
+ * If supplier.get() returns null, reinitialisation occurs on every get().
+ * </pre>
+ */
+public final class ConcurrentLazyOfWeak<T> implements Supplier<T> {
+
+	private final Supplier<T> supplier;
+	private final Object lock = new Object();
+	private volatile WeakReference<T> valueRef;
+
+	public ConcurrentLazyOfWeak(Supplier<T> supplier) {
+		this.supplier = supplier;
+		this.valueRef = new WeakReference<>(null);
+	}
+
+	@Override
+	public T get() {
+		T value = this.valueRef.get();
+		if (value == null) {
+			// Only one thread can enter this at the same time.
+			// Also ensures visibility to other threads.
+			synchronized (this.lock) {
+				// Another thread could have updated it before.
+				value = this.valueRef.get();
+				if (value == null) {
+					value = this.supplier.get();
+					this.valueRef = new WeakReference<>(value);
+				}
+			}
+		}
+		return value;
+	}
+}
--- GeomAlgeLang/src/main/java/de/dhbw/rahmlab/geomalgelang/_TrufflePrototype/TruffleProgramFactory.java
+++ GeomAlgeLang/src/main/java/de/dhbw/rahmlab/geomalgelang/_TrufflePrototype/TruffleProgramFactory.java
@@ -6,18 +6,14 @@ import java.io.BufferedReader;
 public class TruffleProgramFactory implements iProgramFactory<TruffleProgram> {
 
 	// The factory won't die until there is at least one product left.
-	private static final WeakLazy<AutoClosingFactory<Context, TruffleProgram>> factoryRef
-		= new WeakLazy<>(() -> new AutoClosingFactory<>(() -> new Context(), ctx -> new TruffleProgram(ctx)));
+	private static final ConcurrentLazyOfWeak<AutoClosingFactory<Context, TruffleProgram>> factoryRef
+		= new ConcurrentLazyOfWeak<>(() -> new AutoClosingFactory<>(() -> new Context(), ctx -> new TruffleProgram(ctx)));
 
-	// If a TruffleProgramFactory instance is expected to be very often,
-	// the AutoClosingFactory could be cached in a non-static final variable.
-	public TruffleProgramFactory() {
-
-	}
+	private final AutoClosingFactory<Context, TruffleProgram> factory = factoryRef.get();
 
 	@Override
 	public TruffleProgram parse(BufferedReader sourceReader) {
-		return factoryRef.get().createProduct();
+		return this.factory.createProduct();
 	}
 
 	public void finalize() {
