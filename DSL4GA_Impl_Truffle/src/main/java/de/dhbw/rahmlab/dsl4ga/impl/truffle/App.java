e2b5cd66c3364a8dce3dcc220d820c8b07ad5ac7
Oliver Rettig <Oliver.Rettig@orat.de>
2025-01-24 11:52:24 +0100

    Fix removeObject() bug in the visualization if add() failed.

--- DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/App.java
+++ DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/App.java
@@ -28,11 +28,15 @@ public class App {
 		for (int i = 0;; ++i) {
 			CGARoundPointIPNS pm = new CGARoundPointIPNS(new Point3d(i, 0.3, -0.7));
 			CGAViewObject addedObject = viewer.addCGAObject(pm, "pm");
-			System.out.println("Added " + i);
-			// Thread.sleep(1000);
-			addedObject.remove();
-			System.out.println("Removed " + i);
-			// Thread.sleep(1000);
+			if (addedObject != null){
+				System.out.println("Added " + i);
+				// Thread.sleep(1000);
+				addedObject.remove();
+				System.out.println("Removed " + i);
+				// Thread.sleep(1000);
+			} else {
+				System.out.println("Failed "+i);
+			}
 		}
 	}
 
