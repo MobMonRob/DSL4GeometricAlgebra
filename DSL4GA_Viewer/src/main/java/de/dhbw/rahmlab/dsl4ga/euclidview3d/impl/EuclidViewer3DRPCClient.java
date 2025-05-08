af60f27b4b6db4f72295807e782d37c3c5ba6048
Oliver Rettig <Oliver.Rettig@orat.de>
2025-05-08 06:57:20 +0200

    add viewer readme/todo - further development of viewer to eliminate ConformalGeometricAlgebra package in seperate branch

--- DSL4GA_Viewer/src/main/java/de/dhbw/rahmlab/dsl4ga/euclidview3d/impl/EuclidViewer3DRPCClient.java
+++ DSL4GA_Viewer/src/main/java/de/dhbw/rahmlab/dsl4ga/euclidview3d/impl/EuclidViewer3DRPCClient.java
@@ -24,6 +24,17 @@ public class EuclidViewer3DRPCClient implements iEuclidViewer3D {
         viewerRPCService = reference.get();
     }
 	
+	// SEVERE: null
+    // org.apache.dubbo.rpc.RpcException: Failed to invoke the method open in the service de.orat.view3d.   
+	// euclid3dviewapi.spi.iEuclidViewer3D. Tried 3 times of the providers [localhost:50054] (1/1) from the 
+	// registry localhost:50054 on the consumer 10.172.28.211 using the dubbo version 3.3.0. Last error is: 
+	// Invoke remote method timeout. method: open, provider: dubbo://localhost:50054/de.orat.view3d.
+	// euclid3dviewapi.spi.iEuclidViewer3D?application=DEFAULT_DUBBO_APP&background=false&executor-management-
+	// mode=isolation&file-cache=true&interface=de.orat.view3d.euclid3dviewapi.spi.
+	// iEuclidViewer3D&pid=915910&register.ip=10.172.28.211&revision=1.0-
+	// SNAPSHOT&side=consumer&sticky=false&unloadClusterRelated=false, cause: Timeout after 1000ms waiting 
+    // for result.
+
 	@Override
 	public void open() throws Exception {
 		throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
