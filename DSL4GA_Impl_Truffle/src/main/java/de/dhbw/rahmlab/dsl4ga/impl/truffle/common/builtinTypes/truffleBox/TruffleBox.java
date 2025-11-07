55aaf7e0aa2a7ab5799ebbe82ee5ec03bcdd17d5
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-11-07 16:51:21 +0100

    Minor improvements

--- DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/common/builtinTypes/truffleBox/TruffleBox.java
+++ DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/common/builtinTypes/truffleBox/TruffleBox.java
@@ -1,6 +1,5 @@
 package de.dhbw.rahmlab.dsl4ga.impl.truffle.common.builtinTypes.truffleBox;
 
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.interop.TruffleObject;
 
 public class TruffleBox<T> implements TruffleObject {
@@ -11,7 +10,6 @@ public class TruffleBox<T> implements TruffleObject {
 		this.inner = t;
 	}
 
-	@TruffleBoundary
 	public T getInner() {
 		return inner;
 	}
