9306b920ac566cd50ddce62444a938ea8666c024
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-12-19 17:24:29 +0100

    Fix PgaTest

--- DSL4GA_Test/src/test/java/de/dhbw/rahmlab/dsl4ga/test/gafiles/Pgatest.java
+++ DSL4GA_Test/src/test/java/de/dhbw/rahmlab/dsl4ga/test/gafiles/Pgatest.java
@@ -1,14 +1,11 @@
 package de.dhbw.rahmlab.dsl4ga.test.gafiles;
 
 import de.dhbw.rahmlab.dsl4ga.test.gafiles.common.Util;
-import de.dhbw.rahmlab.dsl4ga.test.gafiles.common.gen.fastwrapper.Ik2Program;
 import de.dhbw.rahmlab.dsl4ga.test.gafiles.common.gen.fastwrapper.PgatestProgram;
-import de.dhbw.rahmlab.dsl4ga.test.gafiles.common.gen.fastwrapper.Test1Program;
-import de.orat.math.sparsematrix.SparseDoubleMatrix;
+import de.orat.math.sparsematrix.ColumnVectorSparsity;
+import de.orat.math.sparsematrix.SparseDoubleColumnVector;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import util.cga.SparseCGAColumnVector;
 
 public class Pgatest {
 
@@ -24,12 +21,15 @@ public class Pgatest {
 	@Test
 	void dummy() {
 		System.out.println("Create args:");
-		var a = SparseCGAColumnVector.createScalar(2);
-		var b = SparseCGAColumnVector.createScalar(3);
-		var c = SparseCGAColumnVector.createScalar(3);
-		var d = SparseCGAColumnVector.createScalar(3);
-		var vec1 = SparseCGAColumnVector.createEuclid(new double[]{5d,5d,5d});
-		var vec2 = SparseCGAColumnVector.createEuclid(new double[]{0d,0d,0d});
+		var scalarSparsity = new ColumnVectorSparsity(16, new int[]{0});
+		var euclidSparsity = new ColumnVectorSparsity(16, new int[]{1, 2, 3});
+
+		var a = new SparseDoubleColumnVector(scalarSparsity, new double[]{2});
+		var b = new SparseDoubleColumnVector(scalarSparsity, new double[]{3});
+		var c = new SparseDoubleColumnVector(scalarSparsity, new double[]{3});
+		var d = new SparseDoubleColumnVector(scalarSparsity, new double[]{3});
+		var vec1 = new SparseDoubleColumnVector(euclidSparsity, new double[]{5, 5, 5});
+		var vec2 = new SparseDoubleColumnVector(euclidSparsity, new double[]{0, 0, 0});
 		System.out.println("Invoke:");
 		var answer = PROGRAM.invoke(a,b,c,d, vec1, vec2);
 		Util.print(answer);
