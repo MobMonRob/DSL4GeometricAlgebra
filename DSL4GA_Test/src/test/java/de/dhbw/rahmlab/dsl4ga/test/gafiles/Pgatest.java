6f1e34fb1f58c8ecae001e5220eb40a01756b2c8
Oliver Rettig <Oliver.Rettig@orat.de>
2026-08-11 10:41:52 +0200

    example forward kinematics with PGA completed

--- DSL4GA_Test/src/test/java/de/dhbw/rahmlab/dsl4ga/test/gafiles/Pgatest.java
+++ DSL4GA_Test/src/test/java/de/dhbw/rahmlab/dsl4ga/test/gafiles/Pgatest.java
@@ -24,14 +24,29 @@ public class Pgatest {
 		var scalarSparsity = new ColumnVectorSparsity(16, new int[]{0});
 		var euclidSparsity = new ColumnVectorSparsity(16, new int[]{1, 2, 3});
 
+		// scalar values to define a plane
 		var a = new SparseDoubleColumnVector(scalarSparsity, new double[]{2});
 		var b = new SparseDoubleColumnVector(scalarSparsity, new double[]{3});
 		var c = new SparseDoubleColumnVector(scalarSparsity, new double[]{3});
 		var d = new SparseDoubleColumnVector(scalarSparsity, new double[]{3});
-		var vec1 = new SparseDoubleColumnVector(euclidSparsity, new double[]{5, 5, 5});
-		var vec2 = new SparseDoubleColumnVector(euclidSparsity, new double[]{0, 0, 0});
+		
+		var vec2 = new SparseDoubleColumnVector(euclidSparsity, new double[]{5, 5, 5});
+		var vec1 = new SparseDoubleColumnVector(euclidSparsity, new double[]{0, 0, 0});
 		System.out.println("Invoke:");
 		var answer = PROGRAM.invoke(a,b,c,d, vec1, vec2);
 		Util.print(answer);
+		/* 
+		answer: 1, e0, e1, e2, e3, e01, e02, e03, e12, e13, e23, e012, e013, e023, e123, e0123
+		{
+		  {0.0},{0.0},{0.0},{0.0},{0.0},{0.0},{0.0},{0.0},{0.0},{0.0},{0.0},{0.0},
+		  {-15.0}, {15.0}, {25.0}, {0.0}
+		    e013     e023   e123
+		}
+		
+		nach undual:
+		{0.0}, {25.0}, {-15.0}, {-15.0},...
+		*/
+		//TODO decompose the homogeneous point and compare it with -3/7(1,1,1)
+		// assert if it does not fit
 	}
 }
