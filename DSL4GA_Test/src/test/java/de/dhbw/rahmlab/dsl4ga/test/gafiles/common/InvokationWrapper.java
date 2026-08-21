d18e4bb47959d8c5378227715be726955f996d0f
codedeedope <26957536+codedeedope@users.noreply.github.com>
2026-08-21 13:41:02 +0200

    Renamed FastWrapper to InvokationWrapper

--- DSL4GA_Test/src/test/java/de/dhbw/rahmlab/dsl4ga/test/gafiles/common/InvokationWrapper.java
+++ DSL4GA_Test/src/test/java/de/dhbw/rahmlab/dsl4ga/test/gafiles/common/InvokationWrapper.java
@@ -1,27 +1,23 @@
 package de.dhbw.rahmlab.dsl4ga.test.gafiles.common;
 
 import de.dhbw.rahmlab.dsl4ga.annotation.api.GAFILES;
-import de.dhbw.rahmlab.dsl4ga.impl.fast.api.FastProgramFactory;
 import de.dhbw.rahmlab.dsl4ga.impl.truffle.api.TruffleProgramFactory;
 import de.orat.math.sparsematrix.SparseDoubleMatrix;
-//import de.orat.math.sparsematrix.SparseDoubleColumnVector;
 import java.util.List;
-
-// TODO
-// das FastWrapper ist missverständlich, Name sollte neutral für FAST/Truffle sein
 
 // @GAFILES(value = FastProgramFactory.class, path = "../../")
 @GAFILES(value = TruffleProgramFactory.class, path = "../../")
-public interface FastWrapper {
+public interface InvokationWrapper {
+
 	List<SparseDoubleMatrix> ik2(SparseDoubleMatrix p, SparseDoubleMatrix ae);
 
 	List<SparseDoubleMatrix> test1(SparseDoubleMatrix a, SparseDoubleMatrix b);
+
 	List<SparseDoubleMatrix> ik(SparseDoubleMatrix p, SparseDoubleMatrix ae);
-	
+
 	List<SparseDoubleMatrix> pgatest(SparseDoubleMatrix a, SparseDoubleMatrix b,
-	SparseDoubleMatrix c,
+		SparseDoubleMatrix c,
 		SparseDoubleMatrix d,
 		SparseDoubleMatrix vec1,
 		SparseDoubleMatrix vec2);
-	
 }
