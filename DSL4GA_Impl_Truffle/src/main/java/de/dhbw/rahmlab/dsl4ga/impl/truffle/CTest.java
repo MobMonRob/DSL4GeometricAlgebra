c25028a905c8a6a5600e67979cc22a60a387996a
codedeedope <26957536+codedeedope@users.noreply.github.com>
2026-09-16 14:52:39 +0200

    Added c generation scaffolding.

---
+++ DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/CTest.java
@@ -0,0 +1,20 @@
+package de.dhbw.rahmlab.dsl4ga.impl.truffle;
+
+import de.dhbw.rahmlab.dsl4ga.impl.truffle.api.TruffleProgramFactory;
+
+public class CTest {
+
+	public static void main(String[] args) {
+		String path = "./ctest.ocga";
+		var url = CTest.class.getResource(path);
+		if (url == null) {
+			throw new RuntimeException(String.format("Path not found: %s", path));
+		}
+
+		var fac = new TruffleProgramFactory();
+		var prog = fac.parse(url);
+		var func = prog.invokeSymAsFunction();
+		// "" as path: output will be in DSL4G directory.
+		func.generateC("", "gen.c");
+	}
+}
