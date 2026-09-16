b4709dc22fa56bcfff5cc31f41d362258a4ebe1b
codedeedope <26957536+codedeedope@users.noreply.github.com>
2026-09-16 16:21:58 +0200

    Made types explicit

--- DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/CTest.java
+++ DSL4GA_Impl_Truffle/src/main/java/de/dhbw/rahmlab/dsl4ga/impl/truffle/CTest.java
@@ -1,19 +1,22 @@
 package de.dhbw.rahmlab.dsl4ga.impl.truffle;
 
+import de.dhbw.rahmlab.dsl4ga.impl.truffle.api.TruffleProgram;
 import de.dhbw.rahmlab.dsl4ga.impl.truffle.api.TruffleProgramFactory;
+import de.orat.math.gacalc.api.GAFunction;
+import java.net.URL;
 
 public class CTest {
 
 	public static void main(String[] args) {
 		String path = "./ctest.ocga";
-		var url = CTest.class.getResource(path);
+		URL url = CTest.class.getResource(path);
 		if (url == null) {
 			throw new RuntimeException(String.format("Path not found: %s", path));
 		}
 
-		var fac = new TruffleProgramFactory();
-		var prog = fac.parse(url);
-		var func = prog.invokeSymAsFunction();
+		TruffleProgramFactory fac = new TruffleProgramFactory();
+		TruffleProgram prog = fac.parse(url);
+		GAFunction func = prog.invokeSymAsFunction();
 		// "" as path: output will be in DSL4GA_Impl_Truffle directory.
 		func.generateC("", "gen.c");
 	}
