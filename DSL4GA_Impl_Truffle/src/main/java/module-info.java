4e3eecb39b82a1bd9bfdb3246e604f8f08372e4d
Oliver Rettig <Oliver.Rettig@orat.de>
2026-09-21 15:56:37 +0200

    module-info added

--- DSL4GA_Impl_Truffle/src/main/java/module-info.java
+++ DSL4GA_Impl_Truffle/src/main/java/module-info.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
- */
-
 module DSL4GA_Impl_Truffle {
 	requires DSL4GA.API;
 	requires SparseMatrix;
